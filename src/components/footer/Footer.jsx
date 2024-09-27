edbb11478de76bdf72b9ffda73e423a6c325af9b
milenav <milena.n.valcheva@gmail.com>
2024-09-27 19:14:57 +0300

    Style section Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,42 +1,38 @@
 import './Footer.scss';
 import footerData from './../../footer.json';
-
-// import youTube from '/public/you-tube.png'
-// import fb from '/public/fb.png'
+import youTube from '../../assets/images/you-tube.png';
+import fb from '../../assets/images/fb.png';
 
 const Footer = () => {
-
     return (
         <footer className="footer container-fluid">
             <ul className="container row nav">
                 {footerData.map((item, index) => (
                     <li key={index} className="nav-item">
-                         <span>{item.title}</span>
-                        <ul className="nav">
-                            <li>
-                                <a className="nav-link" href="">Premier R6</a>
-                            </li>
-                            <li>
-                                <a className="nav-link" href="">Premier R6</a>
-                            </li>
-                            <li>
-                                <a className="nav-link" href="">Premier R6</a>
-                            </li>
-                            <li>
-                                <a className="nav-link" href="">Premier R6</a>
-                            </li>
-                        </ul>
+                        <span>{item.title}</span>
+                        {item.submenu && ( // Check if submenu exists
+                            <ul className="nav">
+                                {item.submenu.map((submenu, subIndex) => (
+                                    <li key={subIndex}>
+                                        <a className="nav-link" href={submenu.url}>{submenu.title}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </li>
                 ))}
-                <div>
-                    {/*tuk dvete imgs*/}
-                </div>
             </ul>
+
+            <div className="social-media">
+                <img src={youTube} alt="YouTube"/>
+                <img src={fb} alt="Facebook"/>
+            </div>
+
             <div className="copyright">
                 <p>&copy; 2024 Euro Games Technology All rights reserved.</p>
             </div>
         </footer>
     );
-}
+};
 
 export default Footer;
