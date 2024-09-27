ffcf41df45bc166322226fe0314c60b842b047d4
milenav <milena.n.valcheva@gmail.com>
2024-09-27 16:09:04 +0300

    Change News section

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,12 +1,15 @@
 import './../../App.scss';
 import NewsCard from "./NewsCard";
+import newsData from './../../news.json';
 
 const News = () => {
     return (
         <section className="news-section container-fluid">
             <div className="container">
                 <div className="row news-card-wrapper">
-                    <NewsCard />
+                    {newsData.map((card, index) =>
+                        <NewsCard key={index} title={card.title} src={card.src} image={card.image} alt={card.alt}
+                                  text={card.text} date={card.date} description={card.description}/>)}
                 </div>
             </div>
         </section>
--- src/components/news/NewsCard.jsx
+++ src/components/news/NewsCard.jsx
@@ -1,27 +1,21 @@
-import newsData from './../../news.json';
 import './News.scss';
 
-const NewsCard = () => {
-    return (
-        <>
-            {newsData.map((news, index) => (
-                <article key={index} className="news-card col-3 col-sm">
-                    <img src="" alt="" height="60" width="60"/>
-                    <div>
-                        <span>{news.src}</span>
-                        <img src={news.src} alt=""/>
-                        <h3 className="news-title">{news.title}</h3>
-                    </div>
-                    <img className="news-img" src={news.image} alt={news.alt}/>
-                    <h4 className="news-subtitle">{news.text}</h4>
-                    <span className="news-date">{news.date}</span>
-                    <p className="news-descr">{news.description}</p>
-                    <button className="btn">Read more</button>
-                </article>
-            ))}
+const NewsCard = ({title, src, image, alt, text, date, description}) => {
+    return <>
 
-        </>
-    )
+
+        <article className="news-card col-3 col-sm">
+            <div className="icon-wrapper">
+                <img src={src} alt={alt}/>
+                <h3 className="news-title">{title}</h3>
+            </div>
+            <img className="news-img" src={image} alt={alt}/>
+            <h4 className="news-subtitle">{text}</h4>
+            <span className="news-date">{date}</span>
+            <p className="news-descr">{description}</p>
+            <button className="btn">Read more</button>
+        </article>
+    </>
 }
 
 export default NewsCard;
