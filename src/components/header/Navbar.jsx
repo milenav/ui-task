7646f0e3988771743d52dc713ad0c362e65388aa
milenav <milena.n.valcheva@gmail.com>
2024-09-28 14:22:03 +0300

    Change nav structure

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -3,7 +3,6 @@ import menuItems from '../../menu.json';
 import {IoClose, IoMenu} from "react-icons/io5";
 
 const Navbar = () => {
-    const [selectedValue, setSelectedValue] = useState('');
     const [isOpen, setIsOpen] = useState(false);
 
     const firstMenuItems = menuItems.slice(0, menuItems.length - 3);
@@ -18,39 +17,36 @@ const Navbar = () => {
             </div>
 
             <ul className={`nav-list ${isOpen ? 'active' : 'inactive'}`}>
-                {firstMenuItems.map((option, index) => (
-                    <>
-                        <select
-                            value={selectedValue}
-                            onChange={(e) => setSelectedValue(e.target.value)}
-                        >
-                            <option hidden selected>{option.title}</option>
-                            {option.submenu.map((subItem, subIndex) => (
-                                <option className="nav-item" key={subIndex}>
+                {firstMenuItems.map((item, index) => (
+                    <li className="nav-item" key={index}>
+                        <a className="nav-link" href={item.url}>{item.title}</a>
+                        <ul className="submenu">
+                            {item.submenu.map((subItem, subIndex) => (
+                                <li className="nav-item" key={subIndex}>
                                     <a className="nav-link" href={subItem.url}>{subItem.title}</a>
-                                </option>
+                                </li>
                             ))}
-                        </select>
-                    </>
+                        </ul>
+                    </li>
                 ))}
+            </ul>
 
-                <ul className="nav-list nav-list-right">
-                    {lastMenuItems.map((item, index) => (
-                        <li className="nav-item" key={index}>
-                            <a className="nav-link" href={item.url}>{item.title}</a>
-                            {item.submenu && selectedValue === index && (
-                                <ul className="nav-list">
-                                    {item.submenu.map((subItem, subIndex) => (
-                                        <li className="nav-item" key={subIndex}>
-                                            <a className="nav-link" href={subItem.url}>{subItem.title}</a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </li>
-                    ))}
-                    <li>EN</li>
-                </ul>
+            <ul className="nav-list nav-list-right">
+                {lastMenuItems.map((item, index) => (
+                    <li className="nav-item" key={index}>
+                        <a className="nav-link" href={item.url}>{item.title}</a>
+                        {item.submenu === index && (
+                            <ul className="nav-list">
+                                {item.submenu.map((subItem, subIndex) => (
+                                    <li className="nav-item" key={subIndex}>
+                                        <a className="nav-link" href={subItem.url}>{subItem.title}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </li>
+                ))}
+                <li>EN</li>
             </ul>
         </nav>
     );
